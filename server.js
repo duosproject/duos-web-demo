07eb68c30dcfb8b5fcb5da905316ba4c11899429
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2017-02-14 20:35:37 -0600

    Added base of SPA

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 9000;
+var app = express();
+
+app.use(morgan('dev'));
+app.use(compression());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.get('/*', function (req, res) {
+    res.redirect('/');
+});
+
+app.listen(port);
+console.log('Server started: ' + port);
