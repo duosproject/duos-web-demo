34646616c774f447e853d0c94789418f202d56fd
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2017-03-28 18:07:29 -0500

    Added route for retrieving all records

---
+++ backend/viewmodels/allRecordsViewModel.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//returns structured collection of records
+module.exports = function (data) {
+    var collection = [];
+
+    data.forEach(function (element) {
+        element.VariableSet.VarSetContains.forEach(function (varSet) {
+            varSet.Variables.forEach(function (variable) {
+                collection.push({
+                    articleTitle: element.Article.articleTitle,
+                    methodologyTitle: element.Methodology.methodName,
+                    datasetName: variable.Dataset.datasetName,
+                    variableName: variable.varName
+                });
+            });
+        });
+    });
+
+    return collection;
+}
